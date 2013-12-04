4ab2f91a41806f6d8240baa60c74a440779d0308
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-12-04 19:29:32 +0000

    - Fix depreciated permissions checks
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/content-monitor@11825 f53fe46e-8c02-459b-9fdd-93e75771b278

--- content-monitor.php
+++ content-monitor.php
@@ -1,17 +1,19 @@
 <?php
 /*
 Plugin Name: Content Monitor
-Plugin URI: http://premium.wpmudev.org/project/content-monitor
+Plugin URI: http://premium.wpmudev.org/project/content-monitor/
 Description: Allows you to monitor your entire site for set words that you define (and get an email whenever they are used) - perfect for educational or high profile sites.
-Version: 1.2.2
-Author: Andrew Billits (Incsub)
+Version: 1.3
+Author: Andrew Billits, Aaron Edwards (Incsub)
 Author URI: http://premium.wpmudev.org
+Text Domain: contentmon
+Domain Path: /languages/
 Network: true
 WDP ID: 12
 */
 
 /* 
-Copyright 2007-2011 Incsub (http://incsub.com)
+Copyright 2007-2014 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
@@ -155,13 +157,8 @@ function content_monitor_post_monitor($post_id, $post) {
 function content_monitor_plug_pages() {
 	global $wp_version, $cm_admin_url;
 
-	if ( version_compare($wp_version, '3.0.9', '>') ) {
-    add_submenu_page('settings.php', __('Content Monitor', 'contentmon'), __('Content Monitor', 'contentmon'), 10, 'content-monitor', 'content_monitor_page_main_output');
-    $cm_admin_url = admin_url('network/settings.php?page=content-monitor');
-  } else {
-    add_submenu_page('ms-admin.php', __('Content Monitor', 'contentmon'), __('Content Monitor', 'contentmon'), 10, 'content-monitor', 'content_monitor_page_main_output');
-    $cm_admin_url = admin_url('ms-admin.php?page=content-monitor');
-  }
+	add_submenu_page('settings.php', __('Content Monitor', 'contentmon'), __('Content Monitor', 'contentmon'), 'manage_network_options', 'content-monitor', 'content_monitor_page_main_output');
+  $cm_admin_url = network_admin_url('settings.php?page=content-monitor');
 }
 
 //------------------------------------------------------------------------//
@@ -171,95 +168,67 @@ function content_monitor_plug_pages() {
 function content_monitor_page_main_output() {
 	global $wpdb, $wp_roles, $current_user, $cm_admin_url;
 
-	if( !is_super_admin() ) {
-		echo "<p>" . __('Nice Try...') . "</p>";  //If accessed properly, this message doesn't appear.
-		return;
+	if( !current_user_can('manage_network_options') ) {
+		wp_die('Nice Try...');  //If accessed properly, this message doesn't appear.
 	}
 	
 	echo '<div class="wrap">';
 	
-	if (isset($_GET['updated'])) {
-		?><div id="message" class="updated fade"><p><?php echo urldecode($_GET['updatedmsg']) ?></p></div><?php
+	if (isset($_POST['content_monitor_email'])) {
+		update_site_option( "content_monitor_email", stripslashes($_POST[ 'content_monitor_email' ]) );
+		update_site_option( "content_monitor_post_monitoring", $_POST[ 'content_monitor_post_monitoring' ] );
+		update_site_option( "content_monitor_bad_words", stripslashes($_POST[ 'content_monitor_bad_words' ]) );
+		?><div id="message" class="updated fade"><p><?php _e('Settings saved.', 'contentmon') ?></p></div><?php
 	}
 	
-
-	switch( $_GET[ 'action' ] ) {
-		//---------------------------------------------------//
-		default:
-			?>
-			<h2><?php _e('Content Monitor', 'contentmon') ?></h2>
-        <form method="post" action="<?php echo $cm_admin_url; ?>&action=update">
-        <table class="form-table">
-        <tr valign="top">
-        <th scope="row"><?php _e('Email Address', 'contentmon') ?></th>
-        <td>
-        <input name="content_monitor_email" type="text" id="content_monitor_email" style="width: 95%" value="<?php echo get_site_option('content_monitor_email') ?>" size="45" />
-        <br /><?php _e('Content notifications will be sent to this address. If left blank the site admin email will be used.', 'contentmon') ?></td>
-        </tr>
-        <tr valign="top">
-        <th scope="row"><?php _e('Post/Page Monitoring', 'contentmon') ?></th>
-        <td>
-        <select name="content_monitor_post_monitoring" id="content_monitor_post_monitoring">
-        <?php
-        if (get_site_option('content_monitor_post_monitoring') == '1'){
-        ?>
-        <option value="1" selected="selected"><?php _e('Enabled', 'contentmon') ?></option>
-        <option value="0"><?php _e('Disabled', 'contentmon') ?></option>
-        <?php
-        } else {
-        ?>
-        <option value="1"><?php _e('Enabled', 'contentmon') ?></option>
-        <option value="0" selected="selected"><?php _e('Disabled', 'contentmon') ?></option>
-        <?php
-        }
-        ?>
-        </select>
-        </td>
-        </tr>
-        <tr valign="top">
-        <th scope="row"><?php _e('Bad Words', 'contentmon') ?></th>
-        <td>
-        <textarea name="content_monitor_bad_words" type="text" rows="5" wrap="soft" id="content_monitor_bad_words" style="width: 95%"/><?php echo get_site_option('content_monitor_bad_words') ?></textarea>
-        <br /><?php _e('Place a comma between each word (ex bad, word).', 'contentmon') ?></td>
-        </tr>
-        </table>
-        
-        <p class="submit">
-        <input type="submit" name="Submit" value="<?php _e('Save Changes', 'contentmon') ?>" />
-        </p>
-        </form>
-			<?php
-		break;
-		//---------------------------------------------------//
-		case "update":
-			update_site_option( "content_monitor_email", stripslashes($_POST[ 'content_monitor_email' ]) );
-			update_site_option( "content_monitor_post_monitoring", $_POST[ 'content_monitor_post_monitoring' ] );
-			update_site_option( "content_monitor_bad_words", stripslashes($_POST[ 'content_monitor_bad_words' ]) );
-
-			echo "
-			<SCRIPT LANGUAGE='JavaScript'>
-			window.location='$cm_admin_url&updated=true&updatedmsg=" . urlencode(__('Settings saved.', 'contentmon')) . "';
-			</script>
-			";
-		break;
-		//---------------------------------------------------//
-		case "temp":
-		break;
-		//---------------------------------------------------//
-	}
+	?>
+	<h2><?php _e('Content Monitor', 'contentmon') ?></h2>
+		<form method="post" action="">
+		<table class="form-table">
+		<tr valign="top">
+		<th scope="row"><?php _e('Email Address', 'contentmon') ?></th>
+		<td>
+		<input name="content_monitor_email" type="text" id="content_monitor_email" style="width: 95%" value="<?php echo get_site_option('content_monitor_email') ?>" size="45" />
+		<br /><?php _e('Content notifications will be sent to this address. If left blank the site admin email will be used.', 'contentmon') ?></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row"><?php _e('Post/Page Monitoring', 'contentmon') ?></th>
+		<td>
+		<select name="content_monitor_post_monitoring" id="content_monitor_post_monitoring">
+		<?php
+		if (get_site_option('content_monitor_post_monitoring') == '1'){
+		?>
+		<option value="1" selected="selected"><?php _e('Enabled', 'contentmon') ?></option>
+		<option value="0"><?php _e('Disabled', 'contentmon') ?></option>
+		<?php
+		} else {
+		?>
+		<option value="1"><?php _e('Enabled', 'contentmon') ?></option>
+		<option value="0" selected="selected"><?php _e('Disabled', 'contentmon') ?></option>
+		<?php
+		}
+		?>
+		</select>
+		</td>
+		</tr>
+		<tr valign="top">
+		<th scope="row"><?php _e('Bad Words', 'contentmon') ?></th>
+		<td>
+		<textarea name="content_monitor_bad_words" type="text" rows="5" wrap="soft" id="content_monitor_bad_words" style="width: 95%"/><?php echo get_site_option('content_monitor_bad_words') ?></textarea>
+		<br /><?php _e('Place a comma between each word (ex bad, word).', 'contentmon') ?></td>
+		</tr>
+		</table>
+		
+		<p class="submit">
+		<input type="submit" name="Submit" value="<?php _e('Save Changes', 'contentmon') ?>" />
+		</p>
+		</form>
+	<?php
+	
 	echo '</div>';
 }
 
 
-///////////////////////////////////////////////////////////////////////////
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-  }
-}
-/* --------------------------------------------------------------------- */
-?>
+global $wpmudev_notices;
+$wpmudev_notices[] = array( 'id'=> 12,'name'=> 'Content Monitor', 'screens' => array( 'settings_page_content-monitor-network' ) );
+include_once( dirname( __FILE__ ) . '/dash-notice/wpmudev-dash-notification.php' );
